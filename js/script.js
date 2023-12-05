be292747bd71b3b62dacbfde6e7fbdf63974f956
nurillo00 <nurillobest33@gmail.com>
2023-12-05 21:18:19 +0500

    JavaScript

---
+++ js/script.js
@@ -0,0 +1,92 @@
+const elTitle = document.querySelector('#title');
+const elWrap = document.querySelector('.wrap');
+const elCardBlue = document.querySelector('#card1');
+const elCardRed = document.querySelector('#card2');
+const elCardWhite = document.querySelector('#card3');
+const elCardBlack = document.querySelector('#card4');
+const elBlueTitle = document.querySelector('#blue');
+const elFirstText = document.querySelector('#first');
+const elBlueLink = document.querySelector('#blue-link');
+const elRedTitle = document.querySelector('#red');
+const elSecondText = document.querySelector('#second');
+const elRedLink = document.querySelector('#red-link');
+const elWhiteTitle = document.querySelector('#white');
+const elThirdText = document.querySelector('#third');
+const elWhiteLink = document.querySelector('#white-link');
+const elBlackTitle = document.querySelector('#black');
+const elFourthText = document.querySelector('#fourth');
+const elBlackLink = document.querySelector('#black-link');
+const elLine = document.querySelector('hr')
+
+
+
+elTitle.style.width = '300px';
+elTitle.style.marginTop = '250px';
+elTitle.style.marginBottom = '50px'
+elTitle.style.textAlign = 'center';
+
+elWrap.style.display = 'flex';
+elWrap.style.gap = '0 15px'
+elWrap.style.width = '1300px'
+elWrap.style.margin = '0 auto';
+
+elCardBlue.style.width = '400px';
+elCardBlue.style.padding = '20px 20px';
+elCardBlue.style.backgroundColor = 'dodgerblue';
+
+elCardRed.style.width = '400px';
+elCardRed.style.padding = '20px 20px';
+elCardRed.style.backgroundColor = 'red';
+
+elCardWhite.style.width = '400px';
+elCardWhite.style.padding = '20px 20px';
+elCardWhite.style.backgroundColor = 'white';
+
+elCardBlack.style.width = '400px';
+elCardBlack.style.padding = '20px 20px';
+elCardBlack.style.backgroundColor = 'black';
+
+elBlueTitle.style.color = 'white';
+elBlueTitle.style.fontSize = '30px';
+elBlueTitle.style.fontWeight = '80';
+
+elFirstText.style.marginBottom = '35px';
+elFirstText.style.color = 'white';
+
+elBlueLink.style.textDecoration = 'none';
+elBlueLink.style.color = 'white';
+elBlueLink.style.fontSize = '18px';
+
+elRedTitle.style.color = 'white';
+elRedTitle.style.fontSize = '30px';
+elRedTitle.style.fontWeight = '80';
+
+elSecondText.style.marginBottom = '35px';
+elSecondText.style.color = 'white';
+
+elRedLink.style.textDecoration = 'none';
+elRedLink.style.color = 'white';
+elRedLink.style.fontSize = '18px';
+
+elWhiteTitle.style.color = 'black';
+elWhiteTitle.style.fontSize = '30px';
+elWhiteTitle.style.fontWeight = '80';
+
+elThirdText.style.marginBottom = '35px';
+elThirdText.style.color = 'black';
+
+elWhiteLink.style.textDecoration = 'none';
+elWhiteLink.style.color = 'black';
+elWhiteLink.style.fontSize = '18px';
+
+elBlackTitle.style.color = 'white';
+elBlackTitle.style.fontSize = '30px';
+elBlackTitle.style.fontWeight = '80';
+
+elFourthText.style.marginBottom = '35px';
+elFourthText.style.color = 'white';
+
+elBlackLink.style.textDecoration = 'none';
+elBlackLink.style.color = 'white';
+elBlackLink.style.fontSize = '18px';
+
